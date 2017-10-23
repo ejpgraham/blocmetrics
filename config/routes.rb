6c4691cc2f66a7a537eb5ac5ef63afb7ebbbf074
ejpgraham <ejpgraham@gmail.com>
2017-10-23 19:24:31 -0400

    Add api for events controller.

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   resources :registered_applications
 
+  namespace :api, defaults: { format: :json } do
+    match '/events', to: 'events#preflight', via: [:options]
+    resources :events, only: [:create]
+  end
+
   root 'welcome#index'
 end
